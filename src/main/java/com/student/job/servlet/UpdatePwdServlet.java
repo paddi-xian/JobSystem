b9858524a97d001ed69d0900a0e6be81a44ad6f7
Paddi娴 <2300862852@qq.com>
2023-12-04 14:19:01 +0800

    1204-审核岗位

--- src/main/java/com/student/job/servlet/UpdatePwdServlet.java
+++ src/main/java/com/student/job/servlet/UpdatePwdServlet.java
@@ -6,6 +6,7 @@ import com.student.job.service.UserService;
 import com.student.job.service.impl.UserServiceImpl;
 import com.student.job.utils.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,33 +14,55 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @WebServlet("/updatePwd")
 public class UpdatePwdServlet extends HttpServlet {
     private SqlSession session = SqlSessionUtil.openSession();
     private UserMapper userMapper = session.getMapper(UserMapper.class);
     private UserService userService = new UserServiceImpl();
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        User user = (User) request.getSession().getAttribute("user");
+        Integer uid = user.getU_id();
+//        Integer uid = (Integer) request.getSession().getAttribute("u_id");
 
-        Integer u_id = Integer.parseInt(request.getParameter("u_id"));
-        String old_pass = request.getParameter("old_pass");
-        String new_pass = request.getParameter("new_pass");
+        String oldPwd = request.getParameter("old_pass");
+        String newPwd = request.getParameter("new_pass");
+        String confirmPwd = request.getParameter("confirmPwd");
 
-        User user = new User();
-        user.setU_id(u_id);
-        user.setU_pass(old_pass);
-        user.setU_pass(new_pass);
+//        // 检查新密码和确认密码是否匹配
+//        if (!newPwd.equals(confirmPwd)) {
+//            // 返回错误消息，密码不匹配
+//            response.getWriter().write("新密码和确认密码不匹配");
+//            return;
+//        }
 
-        boolean result = userService.changPwd(u_id, old_pass, new_pass);
-        if(result){
-            response.getWriter().write("修改成功");
-            System.out.println("修改成功");
-        }else {
-            response.getWriter().write("原密码错误");
-            System.out.println("修改失败");
+        user.setU_pass(oldPwd);
+        // 验证旧密码是否正确
+        User checkUser = userMapper.checkPwd(uid, oldPwd);
+        if (checkUser == null) {
+            // 返回错误消息，旧密码不正确
+            response.getWriter().write("旧密码不正确");
+            return;
         }
+        user.setU_id(uid);
+        user.setU_pass(newPwd);
 
+        // 更新密码
+        int i = userMapper.updatePwd(uid, newPwd);
+        session.commit();
+        if (i > 0) {
+            // 更新成功，重定向或返回成功消息
+            response.sendRedirect("/login.jsp"); // 您可以替换为实际的成功页面路径
+        } else {
+            // 更新失败，返回错误消息
+            response.getWriter().write("fail");
+        }
     }
 }
